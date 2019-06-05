29a287078443a8ca58da6b94df2b2765def05a8b
Sergey Pogrebnyak <pogrebnyak.vetal1996@gmail.com>
2019-06-05 19:06:41 +0300

    first part of project

--- Flyweight/Flyweight/Flyweight/ViewController.swift
+++ Flyweight/Flyweight/Flyweight/ViewController.swift
@@ -9,12 +9,50 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var arrayOfImageView: [UIImage]!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let treeFactory = TreeFactory()
+        arrayOfImageView = [UIImage]()
+        for _ in 0...2000000 {
+            let rect = CGRect(x: Int.random(in: 0...Int(self.view.frame.width-125)), y: Int.random(in: 0...Int(self.view.frame.height-125)), width: 125, height: 125)
+            let view = UIImageView.init(frame: rect)
+            let imageIndex = Int.random(in: 0...2)
+            view.image = treeFactory.getImageTree(TreeType.allCases[imageIndex])
+            arrayOfImageView.append(treeFactory.getImageTree(TreeType.allCases[imageIndex]))
+            //self.view.addSubview(view)
+        }
+        print("app first step launch")
+        var counter = 0
+        for image in arrayOfImageView {
+            if image === arrayOfImageView[0] {
+                counter = counter + 1
+            }
+        }
+        print(counter)
+        print("app launch")
     }
 
 
 }
 
+class TreeFactory {
+    private var treeDictionary = [String: UIImage]()
+    func getImageTree(_ treeType: TreeType) -> UIImage {
+//        if let image = self.treeDictionary[treeType.rawValue] {
+//            return image
+//        }
+        
+        let image = UIImage.init(named: treeType.rawValue)!
+        treeDictionary[treeType.rawValue] = image
+        return image
+    }
+}
+
+enum TreeType: String, CaseIterable {
+    case tree1
+    case tree2
+    case tree3
+}
