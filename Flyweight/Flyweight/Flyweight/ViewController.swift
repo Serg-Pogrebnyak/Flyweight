a2c339832689096c21a7157d957ceea976d7ba78
Sergey Pogrebnyak <pogrebnyak.vetal1996@gmail.com>
2019-06-05 17:20:44 +0300

    empty project

---
+++ Flyweight/Flyweight/Flyweight/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Flyweight
+//
+//  Created by Sergey Pohrebnuak on 6/5/19.
+//  Copyright © 2019 Sergey Pohrebnuak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
